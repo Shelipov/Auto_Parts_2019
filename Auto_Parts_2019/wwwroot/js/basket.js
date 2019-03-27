b27591907392e0b20d0e642dcb6d658baa19d62c
Master <shelipov@hotmail.com>
2019-03-27 10:39:31 +0200

    DebugJS

--- Auto_Parts_2019/wwwroot/js/basket.js
+++ Auto_Parts_2019/wwwroot/js/basket.js
@@ -3,11 +3,11 @@
         _cnt = parseInt($(this).val());
     if (_cnt >= 10) {
         _cnt = 10;
-        //($(this).val(10));
+        ($(this).val(10));
     }
     if (_cnt <= 0) {
         _cnt = 1;
-        //($(this).val(1));
+        ($(this).val(1));
     }
     $.post(
         '/editbasket',
